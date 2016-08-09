b22db7f7135614271f69a143d246df03dada9068
n.m.mobile <hihinuma@gmail.com>
2016-08-10 07:54:48 +0900

    commandArgs.js

--- index.js
+++ index.js
@@ -1,38 +1,11 @@
 /*eslint no-console: 0 */
 'use strict';
+
 (function () {
-  const opts = require('opts');
+  const commandArgs = require('./lib/commandArgs.js');
   console.log('start');
 
-  opts.parse(
-    [
-      /*
-      {
-        long: 'pull',
-        description: 'pull from dropbox'
-      },
-      {
-        long: 'push',
-        description: 'push to dropbox'
-      }
-      */
-    ],
-    true // for  Automatically generate help message
-  );
-  const mode = (() => {
-    const mode = opts.args()[0];
-    if (mode === undefined) {
-      throw new Error('subcommand is none.');
-    } else if (mode === 'push') {
-      return 'push';
-    } else if (mode === 'push'){
-      return 'pull';
-    } else {
-      throw new Error(`unkown subcommand: ${mode}.`);
-    }
-  })();
-
-  console.log(mode);
+  console.log(commandArgs.mode);
   console.log('end');
 
 }());
