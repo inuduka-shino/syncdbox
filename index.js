6fbdbae0d7e9b2319b041157f79e527f52dabace
n.m.mobile <hihinuma@gmail.com>
2016-08-10 08:04:13 +0900

    push/pull module

--- index.js
+++ index.js
@@ -6,6 +6,13 @@
   console.log('start');
 
   console.log(commandArgs.mode);
+  if (commandArgs.mode === 'push') {
+    require('./lib/push.js').start();
+  } else if (commandArgs.mode === 'pull') {
+    require('./lib/pull.js').start();
+  } else {
+    throw new Error(`unkown subcommand ${commandArgs.mode}`);
+  }
   console.log('end');
 
 }());
