8a0518a6b5e0a8245cb790ac4d8fd013e13c784e
nknshm <hihinuma@gmail.com>
2016-08-15 20:43:17 +0900

    sample config

---
+++ config_sample.js
@@ -0,0 +1,14 @@
+/*eslint no-console: 0 */
+'use strict';
+
+module.exports = {
+  shelterFolderPath:'xxxx',
+  packages:[
+    {
+      packageName: 'TEST',
+      solt: 'xxx',
+      targetPath: 'xxxxx',
+      files: ['test1.txt', 'test2.txt']
+    }
+  ]
+};
